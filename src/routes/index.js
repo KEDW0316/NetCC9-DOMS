805763db47289ebb1ec5fdb4ac8c892e83ccb1bb
홍동원 <dongwon@hongdong-won-ui-MacBookAir.local>
2022-09-26 13:40:33 +0900

    add websocket to delete object data

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,10 @@ import { main_map } from "../controllers/mapController.js";
 /* GET home page. */
 router.get("/", main_map);
 
+router.get("/api/delete/demo", function (req, res) {
+  res.status(200).json({
+    message: "call get api demo",
+  });
+});
+
 module.exports = router;
